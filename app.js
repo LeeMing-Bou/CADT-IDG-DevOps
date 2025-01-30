2b2d6083c00f3f80e60035ebd8b812f3c3f6f7c4
LeeMing-Bou <pozzzming123@gmail.com>
2025-01-30 08:45:13 +0700

    fix not auto update on server

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('I am just a chill guy.');
+  res.status(200).send('I am just a chill guy. Sir');
 });
 
 app.get('/user/:id', (req, res) => {
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require("./app.js");
 
 app.listen(2000, () => {
-  console.log("Running on PORT 2000");
+  console.log("This server is running on PORT 2000");
 });
